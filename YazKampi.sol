f4098056aa7d3813fc2e6df1851616a8294d5e37
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-02 14:54:24 +0300

    commit

--- YazKampi.sol
+++ YazKampi.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// owner=@furkanadiiguzel
 
 contract YazKampi is ERC20, Ownable {
     constructor() ERC20("Ozgur Yazilim Kampi", "OYK") {}
