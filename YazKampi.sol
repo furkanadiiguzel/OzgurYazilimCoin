60906f4f1f00ffb24047fa4a1273e7c7f4814a7e
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-02 14:50:56 +0300

    commit

---
+++ YazKampi.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract YazKampi is ERC20, Ownable {
+    constructor() ERC20("Ozgur Yazilim Kampi", "OYK") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
